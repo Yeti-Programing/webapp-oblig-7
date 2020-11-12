56517b2769a21caf52db657b4fbf1e35d75bd353
yeti-programing <dankertsenalexander@gmail.com>
2020-11-12 16:35:12 +0100

    Added so that error is displayed if user is not found

--- API/controller/user/userController.js
+++ API/controller/user/userController.js
@@ -9,5 +9,5 @@ export const getUserByMail = (req, res, next) => {
     const { mail } = req.params;
     const { error, status, data } = get(mail);
     console.log(error,status,data);
-    res.status(status).json({ status, data, params: mail, });
+    res.status(status).json({error, status, data, params: mail, });
 };
