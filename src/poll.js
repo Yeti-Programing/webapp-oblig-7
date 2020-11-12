29a5c3e7e049f331df7fcd7b4f97635d71e2b7e3
Robert Alexander Dankertsen <43729337+Yeti-Programing@users.noreply.github.com>
2020-11-12 14:26:49 +0100

    Add files via upload

---
+++ src/poll.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class poll extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default poll
