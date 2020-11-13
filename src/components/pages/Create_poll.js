aa144f4df5cc0cab2ff0f3a5aaf917615a5639e9
Tasaladden <vsostuen@hotmail.com>
2020-11-13 21:06:01 +0100

    Får lag en ny poll

--- src/components/pages/Create_poll.js
+++ src/components/pages/Create_poll.js
@@ -3,21 +3,41 @@ import { v4 as uuid } from 'uuid';
 import axios from 'axios';
 import { Form, Input, Button } from '../../styled/formStyle';
 
+//Er av og til litt treig, så må prøve til du får en alert
 function buttonClicked(){
     let mail = document.getElementById("mail").value.toLowerCase();
     let question = document.getElementById("pollQ").value;
+    let userid = "";
 
-    axios.post('http://localhost:5000/api/v1/poll', {
-        pollid: uuid(),
-        creatorid: uuid(),
-        question:question,
+    axios.get(`http://localhost:5000/api/v1/signup/${mail}`)
+    .then(function(response)
+    { 
+        let json = JSON.stringify(response);
+        let array = json.split("\"");
+        userid = array[15];
+        
+        axios.post('http://localhost:5000/api/v1/poll', {
+            pollid: uuid(),
+            creatorid: userid,
+            question:question
+        })
+        .then(function(){
+            alert("Din poll har blitt registrert!")
+        })
+        .catch(function(error){
+            alert("Det har oppstått en feil, prøv igjen!")
+        })
+
     })
-    .then(function(){
-        alert("Din poll har blitt registrert!")
-    })
-    .catch(function(error){
-        alert("Det har oppstått en feil, prøv igjen!")
-    })
+    .catch(function(error){ 
+        if(error.toString() === "Error: Request failed with status code 404"){
+        alert(`Mailen du har skrevet, er ikke registrert.\n${error}`);
+        }
+        else{
+        alert(`Noe feil skjedde, prøv på nytt.\n${error}`);
+      }})
+       
+      
 }    
 
 function Create_poll() {
