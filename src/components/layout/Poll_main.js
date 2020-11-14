fce6e6e2bc6b35a4711e40f511bbec920cc73796
yeti-programing <dankertsenalexander@gmail.com>
2020-11-14 14:49:48 +0100

    added check poll button to mail

--- src/components/layout/Poll_main.js
+++ src/components/layout/Poll_main.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import { Form, Input, Button } from '../../styled/formStyle';
 
 function buttonClicked(){
-    
+    console.log("GO was clicked");
+}
+
+function checkClicked(){
+    console.log("Check was clicked");
 }
 
 export class Poll_main extends Component {
     render() {
         return (
             <div>
-            <Form onSubmit={buttonClicked}>
+            <Form>
                     <label>Poll-ID:</label>
                     <br/>
                     <Input type="text" placeholder="ex: 1a2bcd3-45..."></Input>
+                    <br/>
+                    <Button type="button" onClick={checkClicked}>Sjekk Poll-ID</Button>
                     <br/>
                     <label>Din e-post:</label>
                     <br/>
@@ -22,7 +28,7 @@ export class Poll_main extends Component {
                     <br/>
                     <Input type="text" placeholder="Ditt svar"></Input>
                     <br/>
-                    <Button type="submit">GO</Button>
+                    <Button type="button" onClick={buttonClicked}>GO</Button>
             </Form>
             </div>
         )
